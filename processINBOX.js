037a584f7c6806397a7f4f440a268d3aa13fdbea
Jianfeng Yang <littlejack0120@gmail.com>
2022-03-24 12:10:23 -0500

    apply changes for publish

--- processINBOX.js
+++ processINBOX.js
@@ -5,76 +5,79 @@
 
 // select from a list of workspace
 // code from https://actions.getdrafts.com/a/1SG
-let workspaces = Workspace.getAll();
-if (workspaces.length == 0) {
-    alert("No workspaces defined.");
-    context.cancel();
+let f = () => {
+    let workspaces = Workspace.getAll();
+    if (workspaces.length == 0) {
+        alert("No workspaces defined.");
+        return false;
+    }
+
+    let p = Prompt.create();
+    p.title = "Select Workspace";
+    p.message = "Choose workspace to process:";
+
+    let ix = 0;
+    for (let ws of workspaces) {
+        p.addButton(ws.name, ix);
+        ix++;
+    }
+
+    if (!p.show()) {
+        return false;
+    }
+
+    // ----------------------------------------------------
+    // START OF USER DEFINITIONS
+
+    // declare "display names" (for the prompt) and "action names" (from your Actions) in an array:
+    // the last pair does not need a "," after the [] brackets!s
+    let actionArray = [
+        ['skip', ''],
+        ['✅ Todoist inbox lines', 'INBOX line’s'],
+        ['🔗 Todoist URL Task', 'Draft URL Task'],
+        ['🗂 Bookmark to DEVONthink', 'bookmark to DEVONthink'],
+        ['⌨️ Markdown to DEVONthink', 'Markdown to DEVONthink CB'],
+        ['🏷 add tags from category', 'add tag from category'],
+        ['🗑 trash', 'trash']
+        //   ['', ''],
+        //   ['', ''],
+        //   ['', ''],
+      ];
+
+    // END OF USER DEFINITIONS
+    // ----------------------------------------------------
+    let selectedIndex = p.buttonPressed;
+    let inboxWorkspace = workspaces[selectedIndex];
+
+    let actionMap = new Map(actionArray);
+    let inboxDrafts = inboxWorkspace.query("inbox"); // see ref https://scripting.getdrafts.com/classes/workspace#query
+
+    // loop through every draft (show title and prompt for action on it)
+
+    for (inboxDraft of inboxDrafts) {
+        let actionPrompt = new Prompt();
+        actionPrompt.title = "process draft"
+        actionPrompt.message = "draft: " + inboxDraft.displayTitle;
+
+        // add button for all elements in the actionMap
+
+        actionMap.forEach(function(value, key) {
+            actionPrompt.addButton(key, value);
+        })
+        actionPrompt.isCancellable = false;
+        actionPrompt.show();
+
+        let selectedAction = actionPrompt.buttonPressed;
+
+        if (selectedAction == '') {
+            // no action for the given name
+        } else {
+            let action = Action.find(selectedAction);
+            app.queueAction(action, inboxDraft);
+        }
+    }
 }
 
-let p = Prompt.create();
-p.title = "Select Workspace";
-p.message = "Choose workspace to process:";
-
-let ix = 0;
-for (let ws of workspaces) {
-    p.addButton(ws.name, ix);
-    ix++;
-}
-
-if (!p.show()) {
-    context.cancel();
-}
-
-let selectedIndex = p.buttonPressed;
-let ws = workspaces[selectedIndex];
-
-// ----------------------------------------------------
-// START OF USER DEFINITIONS
-
-// declare "display names" (for the prompt) and "action names" (from your Actions) in an array:
-// the last pair does not need a "," after the [] brackets!s
-let actionArray = [
-  ['skip', ''],
-  ['✅ Todoist inbox lines', 'INBOX line’s'],
-  ['🔗 Todoist URL Task', 'Draft URL Task'],
-  ['🗂 Bookmark to DEVONthink', 'bookmark to DEVONthink'],
-  ['⌨️ Markdown to DEVONthink', 'Markdown to DEVONthink CB'],
-  ['🏷 add tags from category', 'add tag from category'],
-  ['🗑 trash', 'trash']
-  //   ['', ''],
-  //   ['', ''],
-  //   ['', ''],
-];
-
-// END OF USER DEFINITIONS
-// ----------------------------------------------------
-
-let actionMap = new Map(actionArray);
-let inboxWorkspace = Workspace.find(ws);
-let inboxDrafts = inboxWorkspace.query("inbox"); // see ref https://scripting.getdrafts.com/classes/workspace#query
-
-// loop through every draft (show title and prompt for action on it)
-
-for (inboxDraft of inboxDrafts) {
-  let actionPrompt = new Prompt();
-  actionPrompt.title = "process draft"
-  actionPrompt.message = "draft: " + inboxDraft.displayTitle;
-
-  // add button for all elements in the actionMap
-
-  actionMap.forEach(function(value, key) {
-    actionPrompt.addButton(key, value);
-  })
-  actionPrompt.isCancellable = false;
-  actionPrompt.show();
-
-  let selectedAction = actionPrompt.buttonPressed;
-
-  if (selectedAction == '') {
-    // no action for the given name
-  } else {
-    let action = Action.find(selectedAction);
-    app.queueAction(action, inboxDraft);
-  }
-
+if (!f()) {
+	context.cancel();
 }
